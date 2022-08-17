383576d4e792beb37fa45196e048bd2af6f18991
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2022-08-17 15:07:13 -0300

    fix: Corrige variáveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL:
-        process.env.NODE_ENV === 'production'
-            ? process.env.REACT_APP_PRODUCTION_SERVER_URL
-            : process.env.REACT_APP_DEV_SERVER_URL,
+    baseURL: import.meta.env.PROD
+        ? import.meta.env.VITE_PRODUCTION_SERVER_URL
+        : import.meta.env.VITE_DEV_SERVER_URL,
 });
